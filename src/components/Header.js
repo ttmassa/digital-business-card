c20fabc46e441d405fcda5551762e747820d4867
Massa Timothée <timothee.massa.perso@gmail.com>
2023-07-24 22:03:56 +0200

    Header brut html added

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header>
+            <h1>ReacTeam</h1>
+            <img />
+            <hr/>
+        </header> 
+    );
+}
