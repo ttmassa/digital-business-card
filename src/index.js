c8a4e2aa622fa4b2593849e32da36481720ddad6
Massa Timothée <timothee.massa.perso@gmail.com>
2023-07-24 21:58:37 +0200

    Components initialized

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+/* 
+
+    Here is the model to follow : 
+        <App />
+            <Header />
+
+            <Main />
+                <Title />
+                <Description />
+                <Links />
+            <Main />
+        <App />
+
+*/
