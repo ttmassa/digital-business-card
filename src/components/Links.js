03b18b8acb33185c5223f3220109293ebb50af73
Massa Timothée <timothee.massa.perso@gmail.com>
2023-07-25 14:53:47 +0200

    Links' HTML written

---
+++ src/components/Links.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Links() {
+    return (
+        <article>
+            <ul>
+                <li>timothee.massa.perso@gmail.com</li>
+                <li>My github account</li>
+                <li>Connect with me on Linkedln</li>
+                <li>Paris</li>
+            </ul>
+        </article>
+    )
+}
