03cf16d12bdca3e94b6ef5ed8ce311ea183f5e7d
Massa Timothée <timothee.massa.perso@gmail.com>
2023-07-25 14:35:01 +0200

    ReactTeam icon added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import reacTeamLogo from '../images/reacTeam-icon.svg'
 
 export default function Header() {
     return (
         <header>
             <h1>ReacTeam</h1>
-            <img />
+            <img src={reacTeamLogo} width={120}/>
             <hr/>
         </header> 
     );
