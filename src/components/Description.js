a908b92ac2818089075ffa078a7be93d2d60a559
Massa Timothée <timothee.massa.perso@gmail.com>
2023-07-25 14:46:36 +0200

    Description + Qualifications HTML written

---
+++ src/components/Description.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Description() {
+    return (
+        <div>
+            <h3>Timothée Massa</h3>
+            <h5>Software engineer</h5>
+        </div>
+    );
+}
---
+++ src/components/Qualifications.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Qualifications() {
+    return (
+        <nav>
+            <ul>
+                <li>AWS Certified Developer</li>
+                <li>CompTIA Security+</li>
+                <li>CSD</li>
+            </ul>
+        </nav>
+    );
+}
