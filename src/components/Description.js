09949e15af1ecdac87bf6072b0565838e411bf7b
Massa Timothée <timothee.massa.perso@gmail.com>
2023-07-27 00:58:13 +0200

    Font added

--- src/components/Description.js
+++ src/components/Description.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Description() {
     return (
-        <div>
+        <div className='description'>
             <h3 className='description--name'>Timothée Massa</h3>
             <h5>Software engineer</h5>
         </div>
