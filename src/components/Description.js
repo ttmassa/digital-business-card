f78865c0a575d303aa55afdb3447727aca96970a
Massa Timothée <timothee.massa.perso@gmail.com>
2023-07-27 13:46:42 +0200

    Description styled

--- src/components/Description.js
+++ src/components/Description.js
@@ -4,7 +4,7 @@ export default function Description() {
     return (
         <div className='description'>
             <h3 className='description--name'>Timothée Massa</h3>
-            <h5>Software engineer</h5>
+            <h4 className='description--job'>Software engineer</h4>
         </div>
     );
 }
