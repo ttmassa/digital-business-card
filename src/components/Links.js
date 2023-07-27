ee07fef978534a25cb8fb161994b7aecc0060966
Massa Timothée <timothee.massa.perso@gmail.com>
2023-07-27 14:25:29 +0200

    href added to links

--- src/components/Links.js
+++ src/components/Links.js
@@ -3,11 +3,11 @@ import React from 'react'
 export default function Links() {
     return (
         <article>
-            <ul>
-                <li>timothee.massa.perso@gmail.com</li>
-                <li>My github account</li>
-                <li>Connect with me on Linkedln</li>
-                <li>Paris</li>
+            <ul className='links--list'>
+                <li><a href='mailto:timothee.massa.perso@gmail.com' target='_blank' rel="noreferrer">timothee.massa.perso@gmail.com</a></li>
+                <li><a href='https://github.com/ttmassa' target='_blank' rel="noreferrer">My github account</a></li>
+                <li><a href='https://www.linkedin.com/in/timoth%C3%A9e-massa-88bbbb278/' target='_blank' rel="noreferrer">Connect with me on Linkedln</a></li>
+                <li><a href='https://www.google.com/maps?rlz=1C1VDKB_frFR1019FR1019&output=search&q=paris&source=lnms&entry=mc&sa=X&ved=2ahUKEwjN55jA8K6AAxVMRqQEHRNDBV0Q0pQJegQICxAB' rel="noreferrer">Paris</a></li>
             </ul>
         </article>
     )
