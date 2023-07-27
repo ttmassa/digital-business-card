e66cd7019fc391630524f7d76e1fbb6b707fd40b
Massa Timothée <timothee.massa.perso@gmail.com>
2023-07-27 14:46:14 +0200

    links styled and icon added

--- src/components/Links.js
+++ src/components/Links.js
@@ -4,10 +4,30 @@ export default function Links() {
     return (
         <article>
             <ul className='links--list'>
-                <li><a href='mailto:timothee.massa.perso@gmail.com' target='_blank' rel="noreferrer">timothee.massa.perso@gmail.com</a></li>
-                <li><a href='https://github.com/ttmassa' target='_blank' rel="noreferrer">My github account</a></li>
-                <li><a href='https://www.linkedin.com/in/timoth%C3%A9e-massa-88bbbb278/' target='_blank' rel="noreferrer">Connect with me on Linkedln</a></li>
-                <li><a href='https://www.google.com/maps?rlz=1C1VDKB_frFR1019FR1019&output=search&q=paris&source=lnms&entry=mc&sa=X&ved=2ahUKEwjN55jA8K6AAxVMRqQEHRNDBV0Q0pQJegQICxAB' rel="noreferrer">Paris</a></li>
+                <li>
+                    <a href='mailto:timothee.massa.perso@gmail.com' target='_blank' rel="noreferrer"> 
+                        <div className='links--details-icons'></div>
+                        <div className='links--details'>timothee.massa.perso@gmail.com</div>
+                    </a>
+                </li>
+                <li>
+                    <a href='https://github.com/ttmassa' target='_blank' rel="noreferrer">
+                        <div className='links--details-icons'></div>
+                        <div className='links--details'>My github account</div>
+                    </a>
+                </li>
+                <li>
+                    <a href='https://www.linkedin.com/in/timoth%C3%A9e-massa-88bbbb278/' target='_blank' rel="noreferrer">
+                        <div className='links--details-icons'></div>
+                        <div className='links--details'>Connect with me on Linkedln</div>
+                    </a>
+                </li>
+                <li>
+                    <a href='https://www.google.com/maps?rlz=1C1VDKB_frFR1019FR1019&output=search&q=paris&source=lnms&entry=mc&sa=X&ved=2ahUKEwjN55jA8K6AAxVMRqQEHRNDBV0Q0pQJegQICxAB' rel="noreferrer">
+                        <div className='links--details-icons'></div>
+                        <div className='links--details'>Paris</div>
+                    </a>
+                </li>
             </ul>
         </article>
     )
