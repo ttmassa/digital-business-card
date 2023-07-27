bc93aea83e3ba5b622f3c8689276306ecf8fe861
Massa Timothée <timothee.massa.perso@gmail.com>
2023-07-27 13:56:16 +0200

    Qualifications styled

--- src/components/Qualifications.js
+++ src/components/Qualifications.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Qualifications() {
     return (
         <nav>
-            <ul>
+            <ul className='qualifications--list'>
                 <li>AWS Certified Developer</li>
                 <li>CompTIA Security+</li>
                 <li>CSD</li>
